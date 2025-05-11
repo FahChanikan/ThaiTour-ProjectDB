6a8e0a4ba24a0c6b2fd02c5ac21df36f73297ab2
chanikan <chanikan.n91@gmail.com>
2025-05-11 18:29:16 +0800

    Fix app.js for host

--- db.js
+++ db.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,        
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,     
-  database: process.env.DB_NAME
-})
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: process.env.DB_PORT, 
+  password: process.env.DB_PASSWORD || 'PZTE8bgi',
+  database: process.env.DB_NAME || 'ThaiTour'
+});
 
 db.connect((err) => {
   if (err) {
