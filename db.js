ca02698cba080975166d1c87d8ef7d3357253923
chanikan <chanikan.n91@gmail.com>
2025-05-11 16:23:43 +0800

    Modify db.js

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'PZTE8bgi',
-  database: process.env.DB_NAME || 'ThaiTour'
-});
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,        
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,     
+  database: process.env.DB_NAME
+})
 
 db.connect((err) => {
   if (err) {
